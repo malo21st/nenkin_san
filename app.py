4125e14fd0b5b12b1218f22ea39068bedde40afe
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-09 23:48:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ import os
 import base64
 
 def show_pdf(file_path):
-    with open(file_path,"rb") as f:
+    with open(Path(file_path),"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 show_pdf('./pdf-pages/R5_tenjikaijyosei_boshuyoko_230403-1.pdf')
