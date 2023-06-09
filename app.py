e5a12e8919226c0ddb9a60cce05c5249387f838c
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-09 09:35:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-from llama_index import download_loader
-from pathlib import Path
+# from llama_index import download_loader
+# from pathlib import Path
 from llama_index import LLMPredictor, GPTVectorStoreIndex, PromptHelper, ServiceContext
 from llama_index import QuestionAnswerPrompt, GPTVectorStoreIndex, SimpleDirectoryReader
+from llama_index import StorageContext, load_index_from_storage
 from langchain import OpenAI
-from tempfile import NamedTemporaryFile
+# from tempfile import NamedTemporaryFile
 import os
 
 os.environ["OPENAI_API_KEY"] = st.secrets.openai_api_key
@@ -26,15 +27,9 @@ QA_PROMPT_TMPL = (
 QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
 
 @st.cache_resource
-def get_vector_db(uploaded_file):
-    with NamedTemporaryFile(dir='.', suffix='.pdf') as f:
-        f.write(uploaded_file.getbuffer())
-        PDFReader = download_loader("PDFReader")
-        loader = PDFReader()
-        documents = loader.load_data(file=Path(f.name))
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True))
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
-    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+def load_vector_db(uploaded_file):
+    storage_context = StorageContext.from_defaults(persist_dir="./storage/")
+    index = load_index_from_storage(storage_context)
     return index
 
 def store_del_msg():
@@ -43,40 +38,35 @@ def store_del_msg():
 
 # View (User Interface)
 ## Sidebar
-st.sidebar.title("PDFWhiz")
-uploaded_file = st.sidebar.file_uploader("PDFファイルをアップロードして下さい", type=["pdf"])
-if uploaded_file is not None:
-    if uploaded_file.name != st.session_state.qa.get("pdf", ""):
-        st.session_state.qa = {"pdf": uploaded_file.name, "history": []}
-    user_input = st.sidebar.text_input("ご質問をどうぞ", key="user_input", on_change=store_del_msg)
-    ## Main Content
-    if st.session_state.qa["history"]:
-        for message in st.session_state.qa["history"]:
+st.sidebar.title("年金さん")
+user_input = st.sidebar.text_input("ご質問をどうぞ", key="user_input", on_change=store_del_msg)
+## Main Content
+if st.session_state.qa["history"]:
+    for message in st.session_state.qa["history"]:
 #         for message in st.session_state["qa"][1:]:
-            if message["role"] == "Q": # Q: Question (User)
-                st.info(message["msg"])
-            elif message["role"] == "A": # A: Answer (AI Assistant)
-                st.write(message["msg"])
-            elif message["role"] == "E": # E: Error
-                st.error(message["msg"])
-    chat_box = st.empty() # Streaming message
+        if message["role"] == "Q": # Q: Question (User)
+            st.info(message["msg"])
+        elif message["role"] == "A": # A: Answer (AI Assistant)
+            st.write(message["msg"])
+        elif message["role"] == "E": # E: Error
+            st.error(message["msg"])
+chat_box = st.empty() # Streaming message
 
-    # Model (Business Logic)
-    index = get_vector_db(uploaded_file)
-#     stream_handler = StreamHandler(chat_box)
-    engine = index.as_query_engine(text_qa_template=QA_PROMPT, streaming=True, similarity_top_k=1)
-    if st.session_state.qa["history"]:
-        query = st.session_state.qa["history"][-1]["msg"]
-        try:
-            response = engine.query(query) # Query to ChatGPT
-            text = ""
-            for next in response.response_gen:
-                text += next
-                chat_box.write(text)
-            refer_pages = "\n\n参照：" + ", ".join([f"{node.extra_info['page_label']}ページ" for node in response.source_nodes])
-            chat_box.write(text + refer_pages)
-            st.session_state.qa["history"].append({"role": "A", "msg": text + refer_pages})
-        except Exception as error_msg:
+# Model (Business Logic)
+index = load_vector_db(uploaded_file)
+engine = index.as_query_engine(text_qa_template=QA_PROMPT, streaming=True, similarity_top_k=1)
+if st.session_state.qa["history"]:
+    query = st.session_state.qa["history"][-1]["msg"]
+    try:
+        response = engine.query(query) # Query to ChatGPT
+        text = ""
+        for next in response.response_gen:
+            text += next
+            chat_box.write(text)
+        refer_pages = "\n\n参照：" + ", ".join([f"{node.extra_info['page_label']}ページ" for node in response.source_nodes])
+        chat_box.write(text + refer_pages)
+        st.session_state.qa["history"].append({"role": "A", "msg": text + refer_pages})
+    except Exception as error_msg:
 #             error_msg = "エラーが発生しました！　もう一度、質問して下さい。"
-            st.error(error_msg)
-            st.session_state.qa["history"].append({"role": "E", "msg": error_msg})
+        st.error(error_msg)
+        st.session_state.qa["history"].append({"role": "E", "msg": error_msg})
