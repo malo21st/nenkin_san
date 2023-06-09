da94e049d03261f270f170ab9ec48310a66910a9
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-09 17:01:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ with tab_qa:
 
 with tab_doc:
     image = Image.open(f"./pdf_png/{st.session_state.page}.png")
-    st.sidebar.image(image, caption = '展示会出展助成事業（令和５年度　東京都）', use_column_width = "auto")
+    st.image(image, caption = '展示会出展助成事業（令和５年度　東京都）', use_column_width = "auto")
     
 # st.session_state.qa
 st.session_state.page
