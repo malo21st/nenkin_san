b6b6df8538c29cf20c4f460b11639ffbb0b98e86
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-09 16:23:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def store_del_msg():
 st.sidebar.title("補助金さん")
 st.sidebar.write("補助金・助成金についてお任せあれ")
 user_input = st.sidebar.text_input("ご質問をどうぞ", key="user_input", on_change=store_del_msg)
+st.session_state.page = st.sidebar.selectbox("ページ", PAGE_LIST, index=PAGE_LIST.index("page_001"))
 
 # st.sidebar.markdown("---")
 ## Main Content
@@ -85,8 +86,6 @@ if st.session_state.qa["history"][-1]["role"] == "Q":
         st.error(error_msg)
         st.session_state.qa["history"].append({"role": "E", "msg": error_msg})
 
-page = st.sidebar.selectbox("ページ", PAGE_LIST, index=PAGE_LIST.index("page_001"))
-st.session_state.page = page
 image = Image.open(f"./pdf_png/{st.session_state.page}.png")
 st.sidebar.image(image, caption = '展示会出展助成事業（令和５年度　東京都）', use_column_width = "auto")
 
