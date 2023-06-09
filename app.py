74bb87735c5061c0ffffdb27393b61bbe9ec2869
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-10 00:03:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import base64
 
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('shift_jis')
+        base64_pdf = base64.b64encode(f.read()).decode()
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
