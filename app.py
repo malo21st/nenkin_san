974a495cc5066ac0dcd379079012df4f5ddd491c
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-09 20:51:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -96,23 +96,24 @@ if st.session_state.qa["history"][-1]["role"] == "Q":
         st.session_state.qa["history"].append({"role": "E", "msg": error_msg})
 
 with pdf_page:
-    col_l, col_prev, col_next, col_r = st.columns([1.5, 1, 1, 1.5])
-    page = st.session_state.pdf_page
-    with col_prev:
-        if page == 1:
-            st.button("＜ 前ページ", on_click=show_pdf, args=([page]), disabled=True)
-        elif page > 1:
-            st.button("＜ 前ページ", on_click=show_pdf, args=([page-1]))
-    with col_next:
-        if page == 37:
-            st.button("次ページ ＞", on_click=show_pdf, args=([page]), disabled=True)
-        elif page < 37:
-            st.button("次ページ ＞", on_click=show_pdf, args=([page+1]))
-    with col_l: pass
-    with col_r: pass
-    pdf_image = get_pdf_image(st.session_state.pdf_page)
-    st.write(page, st.session_state.pdf_page )
-    st.image(pdf_image, caption = '展示会出展助成事業（令和５年度　東京都）', use_column_width = "auto")
+    st.expander("参照ページを開く")
+        page = st.session_state.pdf_page
+    #     col_l, col_prev, col_next, col_r = st.columns([1.5, 1, 1, 1.5])
+    #     with col_prev:
+    #         if page == 1:
+    #             st.button("＜ 前ページ", on_click=show_pdf, args=([page]), disabled=True)
+    #         elif page > 1:
+    #             st.button("＜ 前ページ", on_click=show_pdf, args=([page-1]))
+    #     with col_next:
+    #         if page == 37:
+    #             st.button("次ページ ＞", on_click=show_pdf, args=([page]), disabled=True)
+    #         elif page < 37:
+    #             st.button("次ページ ＞", on_click=show_pdf, args=([page+1]))
+    #     with col_l: pass
+    #     with col_r: pass
+        pdf_image = get_pdf_image(st.session_state.pdf_page)
+        st.write(page, st.session_state.pdf_page )
+        st.image(pdf_image, caption = '展示会出展助成事業（令和５年度　東京都）', use_column_width = "auto")
 
 # st.session_state.qa
 # st.session_state.pdf_page
