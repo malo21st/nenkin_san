325263b7baa5764d8dfe458637c3e525a297f3a3
malo21st <44739330+malo21st@users.noreply.github.com>
2023-06-09 20:53:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if st.session_state.qa["history"][-1]["role"] == "Q":
         st.session_state.qa["history"].append({"role": "E", "msg": error_msg})
 
 with pdf_page:
-    st.expander("参照ページを開く")
+    with st.expander("参照ページを開く"):
         page = st.session_state.pdf_page
     #     col_l, col_prev, col_next, col_r = st.columns([1.5, 1, 1, 1.5])
     #     with col_prev:
