6f2d5fe86c57d86b430ebdbab0ef9840968fc095
malo21st <44739330+malo21st@users.noreply.github.com>
2023-10-03 15:59:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ from llama_index import StorageContext, load_index_from_storage
 from langchain import OpenAI
 # from langchain.chat_models import ChatOpenAI
 from PIL import Image
+import fitz  # PyMuPDF
 import os
 # import base64
 
 type_to_index = {"展示会出展助成事業":0, "BX2000":1, "MX1000/3000":2}
 type_to_path = {"展示会出展助成事業":"./storage/", "BX2000":"./BX2000_DB/", "MX1000/3000":"./MX1000_DB/"}
+docu_to_pdf_path = {"展示会出展助成事業":"./PDF/R5_tenjikaijyosei_boshuyoko_230403.pdf", "BX2000":"./PDF/BX2000_m.pdf", "MX1000/3000":"./PDF/MX1000_3000_m.pdf"}
 # def show_pdf(file_path):
 #     with open(file_path,"rb") as f:
 #         base64_pdf = base64.b64encode(f.read()).decode()
@@ -58,8 +60,14 @@ def load_vector_db(docu_type):
     return index
 
 @st.cache_data
-def get_pdf_image(page):
-    return Image.open(f"./pdf_png/{PAGE_DIC[page]}")
+def get_pdf_image(docu_type, page):
+    pdf_path = docu_to_pdf_path(cocu_type)
+    pdf_document = fitz.open(pdf_path)
+    page_data = pdf_document.load_page(page)
+    image = page_data.get_pixmap()
+    image_data = image.tobytes("png")
+    return image_data
+    # return Image.open(f"./pdf_png/{PAGE_DIC[page]}")
 
 def show_pdf(page):
     st.session_state.pdf_page = page
@@ -122,8 +130,8 @@ if st.session_state.qa["history"][-1]["role"] == "Q":
         with pdf_page:
             with st.expander(f"{page_int - 1} ページを開く"):
                 page = st.session_state.pdf_page
-                pdf_image = get_pdf_image(st.session_state.pdf_page)
-                st.image(pdf_image, caption = '展示会出展助成事業（令和５年度　東京都）', use_column_width = "auto")
+                pdf_image = get_pdf_image(st.session_state.docu_index, st.session_state.pdf_page)
+                st.image(pdf_image, caption = f'{docu_type} {st.session_state.pdf_page}ページ', use_column_width = "auto")
     except Exception as error_msg:
 #             error_msg = "エラーが発生しました！　もう一度、質問して下さい。"
         st.error(error_msg)
